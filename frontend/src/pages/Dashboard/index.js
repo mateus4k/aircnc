c8461ad30c2674182cc0c8c6d27a3e783c718ecc
Mateus <mateus4k@protonmail.ch>
2019-10-04 20:19:06 -0300

    :rocket: Socket.io! :rocket:

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { Link } from 'react-router-dom'
+import socketio from 'socket.io-client'
 import api from '../../services/api'
 
 import './styles.css'
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([])
+  const [requests, setRequests] = useState([])
+
+  const user_id = localStorage.getItem('user')
+  const socket = useMemo(
+    () =>
+      socketio('http://192.168.0.2:3333', {
+        query: { user_id }
+      }),
+    [user_id]
+  )
+
+  useEffect(() => {
+    socket.on('booking_request', data => {
+      setRequests([...requests, data])
+    })
+  }, [requests, socket])
 
   useEffect(() => {
     async function loadSpots() {
@@ -20,8 +37,43 @@ export default function Dashboard() {
     loadSpots()
   }, [])
 
+  async function handleApprove(id) {
+    await api.post(`/bookings/${id}/approvals`)
+
+    setRequests(requests.filter(request => request._id !== id))
+  }
+
+  async function handleDecline(id) {
+    await api.post(`/bookings/${id}/rejections`)
+
+    setRequests(requests.filter(request => request._id !== id))
+  }
+
   return (
     <>
+      <ul className="notifications">
+        {requests.map(request => (
+          <li key={request._id}>
+            <p>
+              <strong> {request.user.email} </strong>requested a booking on
+              <strong> {request.spot.company} </strong>for the day
+              <strong> {request.date} </strong>
+            </p>
+            <button
+              className="approve"
+              onClick={() => handleApprove(request._id)}
+            >
+              APPROVE
+            </button>
+            <button
+              className="decline"
+              onClick={() => handleDecline(request._id)}
+            >
+              DECLINE
+            </button>
+          </li>
+        ))}
+      </ul>
       <ul className="spot-list">
         {spots.map(spot => (
           <li key={spot._id}>
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
+import socketio from 'socket.io-client'
 import {
+  Alert,
   SafeAreaView,
   ScrollView,
   StyleSheet,
@@ -13,6 +15,22 @@ import logo from '../assets/logo.png'
 
 export default function List() {
   const [techs, setTechs] = useState([])
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const socket = socketio('http://192.168.0.2:3333', {
+        query: { user_id }
+      })
+
+      socket.on('booking_response', booking => {
+        Alert.alert(
+          `Your booking on ${booking.spot.company} on ${booking.date} was ${
+            booking.approved ? 'APPROVE' : 'DECLINE'
+          }`
+        )
+      })
+    })
+  }, [])
 
   useEffect(() => {
     AsyncStorage.getItem('techs').then(storagedTechs => {
